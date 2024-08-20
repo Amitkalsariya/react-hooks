74ab17e40094a571cf94dc5422247575607838b0
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-08-20 16:31:19 +0530

    Done

--- src/Compo/Second.jsx
+++ src/Compo/Second.jsx
@@ -1,12 +1,19 @@
 import React, { useContext, useState } from 'react'
 import usercontext from './First'
 const Second = () => {
-    const {Plus,Minus,count}=useContext(usercontext)
+  const { Plus, Minus, count } = useContext(usercontext)
   return (
     <div>
-        <button onClick={Plus}>+</button>
-        <h1>{count}</h1>
-        <button onClick={Minus}>-</button>
+      <table border={1} cellPadding={2} cellSpacing={5} align='center'>
+        <tr>
+          <th> <button onClick={Plus}>Increment</button></th>
+          <th><h1>{count}</h1></th>
+          <th><button onClick={Minus}>Decrement</button></th>
+        </tr>
+      </table>
+
+
+
     </div>
   )
 }
